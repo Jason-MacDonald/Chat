ae52aa86f223ce76e94a4f024a218a4086b457fb
Jason-MacDonald <49774612+Jason-MacDonald@users.noreply.github.com>
2020-12-01 13:21:34 +1300

    fixed unused variable warning

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Menu />
-      { auth && auth.currentUser ? <ChatRoom /> : null }      
+      { user ? <ChatRoom /> : null }      
     </div>
   );
 }
